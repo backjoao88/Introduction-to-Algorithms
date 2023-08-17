823cda3328280c7e1ea90ab4fa8f95fc41ebeddb
Back <joaoback47@gmail.com>
2023-08-17 07:58:25 -0300

    [add] circular linked list

--- Chapter10/10.2-5.cpp
+++ Chapter10/10.2-5.cpp
@@ -1,60 +1,123 @@
 /**
- *
  * Implemente as operações de dicionário INSERT, DELETE e SEARCH usando listas circulares simplesmente ligadas.
  * Quais são os tempos de execução dos seus procedimentos?
- *
  */
 
-#include <string>
-#include <utility>
 #include <stdio.h>
 
-template <typename T>
+template <typename K, typename V>
 class Node {
    public:
-    T value;
-    Node<T>* next;
-    Node(const T& value) {
-        this->value = value;
-    };
+    Node(const K& c_key, const V& c_value) : key(c_key), value(c_value){};
+    K key;
+    V value;
+    Node<K, V>* next;
 };
 
-template <typename T>
+template <typename K, typename V>
 class CircularLinkedList {
    public:
-    Node<T>* head;
-    void insert(const T& value) {
+    CircularLinkedList() { this->head = nullptr; };
+    Node<K, V>* head;
+
+    void addLast(const K& key, const V& value) {
         if (this->head == nullptr) {
-            this->head = new Node<T>(value);
+            this->head = new Node<K, V>(key, value);
             this->head->next = this->head;
             return;
-        };
-        Node<T>* tmp = this->head;
+        }
+        Node<K, V>* node = new Node<K, V>(key, value);
+        Node<K, V>* tmp = this->head;
         while (tmp->next != this->head) {
             tmp = tmp->next;
-        };
-        tmp->next = new Node<T>(value);
-        tmp->next->next = this->head;
-    };
+        }
+        tmp->next = node;
+        node->next = this->head;
+    }
 
-    void remove(const T& value){
-        Node<T>* tmp = this->head;
-        Node<T>* prev = nullptr;
-        do{
-            prev = tmp;
-            tmp = tmp->next;
-        }while(tmp != this->head && tmp->value != value);
+    void addFront(const K& key, const V& value) {
+        if (this->head == nullptr) {
+            this->head = new Node<K, V>(key, value);
+            this->head->next = this->head;
+            return;
+        }
+        Node<K, V>* tmp = new Node<K, V>(key, value);
+        tmp->next = this->head;
 
-        if(tmp->value == value){
-            prev->next = tmp->next;
-            delete tmp;
+        printf("tmp %p\n", tmp);
+        printf("tmp address %p\n", &tmp);
+        printf("tmp nxt %p - head %p\n", tmp->next, this->head);
+        printf("tmp address nxt %p - head address %p\n", &tmp->next, &this->head);
+        // update last node next ref
+        Node<K, V>* curr = this->head;
+
+        while (curr->next != this->head) {
+            curr = curr->next;
         }
 
-        return;
+        printf("curr next %p\n", curr->next);
+
+        this->head = tmp;
+        printf("new head %p\n", this->head);
+        printf("curr next %p\n", curr->next);
+        curr->next = this->head;
+        printf("curr next %p\n", curr->next);
+        printf("\n");
+    }
+
+    void remove(const K& key) {
+        if (this->head == nullptr) {
+            return;
+        }
+
+        if (this->head->key == key) {
+            Node<K, V>* curr = this->head;
+            while (curr->next != this->head) {
+                curr = curr->next;
+            }
+            this->head = this->head->next;
+            curr->next = this->head;
+            return;
+        }
+
+        Node<K, V>* tmp = this->head;
+        Node<K, V>* prev = nullptr;
+        while (tmp->next != this->head) {
+            if (tmp->key == key) {
+                break;
+            }
+            prev = tmp;
+            tmp = tmp->next;
+        }
+        prev->next = tmp->next;
+    }
+
+    V search(const K& key) {
+        if (this->head == nullptr) {
+            return -1;
+        }
+
+        Node<K, V>* tmp = this->head;
+
+        if (key == tmp->key) {
+            return tmp->value;
+        }
+        while (tmp->next != this->head) {
+            if (key == tmp->key) {
+                return tmp->value;
+            }
+            tmp = tmp->next;
+        }
+
+        return -1;
     }
 
     void show() {
-        Node<T>* tmp = this->head;
+        Node<K, V>* tmp = this->head;
+        if (tmp == nullptr) {
+            printf("empty.\n");
+            return;
+        }
         do {
             printf("%d ", tmp->value);
             tmp = tmp->next;
@@ -63,60 +126,40 @@ class CircularLinkedList {
     }
 };
 
-template <typename K, typename V>
-class Dict{
-    public:
-        CircularLinkedList<K>* keys;
-        CircularLinkedList<V>* values;
-        Dict(){
-            keys = new CircularLinkedList<K>();
-            values = new CircularLinkedList<V>();
-        }
-        void insert(const K& key, const V& value){
-            this->keys->insert(key);
-            this->values->insert(value);
-        }
-
-        void remove(const K& key){
-            this->keys->remove(key);
-        }
-
-        void showValues(){
-            this->values->show();
-        }
-
-        void showKeys(){
-            this->keys->show();
-        }
-};
-
 int main(void) {
-    // CircularLinkedList<int>* circularLinkedList = new CircularLinkedList<int>();
-    // circularLinkedList->insert(5);
-    // circularLinkedList->insert(1);
-    // circularLinkedList->insert(1);
-    // circularLinkedList->insert(1);
-    // circularLinkedList->insert(1);
-    // circularLinkedList->insert(1);
-    // circularLinkedList->insert(1);
+    CircularLinkedList<int, int>* circularLinkedList = new CircularLinkedList<int, int>();
+    circularLinkedList->addFront(44, 2);
     // circularLinkedList->show();
-
-    Dict<int, int>* myDict = new Dict<int, int>();
-     
-    myDict->insert(0, 1);
-    myDict->insert(1, 1);
-    myDict->insert(2, 3);
-    myDict->insert(3, 4);
-    myDict->insert(4, 5);
-    myDict->insert(5, 1);
-    myDict->showKeys();
-    myDict->remove(3);
-    myDict->showKeys();
-    myDict->remove(2);
-    myDict->showKeys();
-    myDict->remove(5);
-    myDict->showKeys();
-    // myDict.remove(1);
-
-    return 0;
-}
+    circularLinkedList->addFront(1, 3);
+    // circularLinkedList->show();
+    circularLinkedList->addFront(2, 4);
+    // circularLinkedList->show();
+    circularLinkedList->addFront(3, 1);
+    // circularLinkedList->show();
+    circularLinkedList->addFront(4, 22);
+    circularLinkedList->addFront(5, 23);
+    circularLinkedList->addFront(6, 24);
+    circularLinkedList->addFront(7, 25);
+    circularLinkedList->addFront(8, 126);
+    circularLinkedList->remove(8);
+    circularLinkedList->show();
+    circularLinkedList->addLast(9, 20);
+    circularLinkedList->addFront(10, 255);
+    circularLinkedList->addLast(11, 256);
+    circularLinkedList->show();
+    printf("%d \n", circularLinkedList->search(7));
+    circularLinkedList->remove(7);
+    circularLinkedList->show();
+    circularLinkedList->remove(44);
+    circularLinkedList->show();
+    circularLinkedList->remove(11);
+    circularLinkedList->show();
+    circularLinkedList->addLast(13, 66);
+    circularLinkedList->show();
+    circularLinkedList->addFront(22, 33);
+    circularLinkedList->show();
+    circularLinkedList->remove(22);
+    circularLinkedList->show();
+    circularLinkedList->remove(10);
+    circularLinkedList->show();
+};
